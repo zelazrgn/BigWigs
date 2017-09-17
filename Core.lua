4f00864455d06a3845f497da6a0a03efd7410f12
Hosq <pohjolainen.topi@gmail.com>
2017-09-17 07:23:23 +0300

    r20021

--- Core.lua
+++ Core.lua
@@ -168,7 +168,7 @@ BigWigs.cmdtable = {type = "group", handler = BigWigs, args = {
 }}
 BigWigs:RegisterChatCommand({"/bw", "/BigWigs"}, BigWigs.cmdtable)
 BigWigs.debugFrame = ChatFrame1
-BigWigs.revision = 20020
+BigWigs.revision = 20021
 
 
 function BigWigs:DebugMessage(msg, module)
